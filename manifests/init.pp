83e63d2e91d4e81053061223638ef3c19bf1a624
Alexander Werner <bundeswerner@googlemail.com>
2010-11-16 02:40:39 +0100

    Various updates

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,97 +1,79 @@
 # ssh/manifests/init.pp - common ssh related components
 # Copyright (C) 2007 David Schmitt <david@schmitt.edv-bus.at>
 # See LICENSE for the full license granted to you.
+# Adapted by Alexander Werner <alex@documentfoundation.org>
 
 class ssh::common {
-	file {
-		"/etc/ssh":
-			ensure => directory,
-			mode => 0755, owner => root, group => root,
-	}
-	group {
-		ssh:
-			gid => 204,
-			allowdupe => false,
-	}
+  file {
+    "/etc/ssh":
+      ensure => directory,
+      mode => 0755, owner => root, group => root,
+  }
 }
 
 class ssh::client inherits ssh::common {
-	package {
-		"openssh-client":
-			ensure => installed,
-			require => [ File["/etc/ssh"], Group[ssh] ],
-	}
+  package {
+    "openssh-client":
+      ensure => installed,
+      require => File["/etc/ssh"],
+  }
+  
 
-	# this is needed because the gid might have changed
-	file {
-		"/usr/bin/ssh-agent":
-			group => ssh,
-			require => Package[openssh-client];
-		"/etc/ssh/ssh_known_hosts":
-			mode => 0644, owner => root, group => 0;
-	}
-	
-	# Now collect all server keys
-	Sshkey <<||>>
+  file {
+    "/etc/ssh/ssh_known_hosts":
+      mode => 0644, owner => root, group => 0;
+  }
+  
+  # Now collect all server keys
+  Sshkey <<||>>
 }
 
 class ssh::server inherits ssh::client {
-
-	package {
-		"openssh-server":
-			ensure => installed,
-			require => [ File["/etc/ssh"], User[sshd] ],
-	}
-
-	user {
-		sshd:
-			uid => 204, gid => 65534,
-			home => "/var/run/sshd",
-			shell => "/usr/sbin/nologin",
-			allowdupe => false,
-	}
-
-	service {
-		ssh:
-			ensure => running,
-			pattern => "sshd",
-			require => Package["openssh-server"],
-			subscribe => [ User[sshd], Group["ssh"] ]
-	}
-
-	# Now add the key, if we've got one
-	case $sshrsakey_key {
-		"": { 
-			err("no sshrsakey on $fqdn")
-		}
-		default: {
-			#@@sshkey { "$hostname.$domain": type => ssh-dss, key => $sshdsakey_key, ensure => present, }
-			debug ( "Storing rsa key for $hostname.$domain" )
-			@@sshkey { "$hostname.$domain":
-				type => ssh-rsa,
-				key => $sshrsakey_key,
-				ensure => present,
-				require => Package["openssh-client"],
-			}
-		}
-	}
-
-	$ssh_port = $ssh_port ? { '' => 22, default => $ssh_port }
-	$ssh_permit_root_login = $ssh_permit_root_login ? {'' => no, default => $ssh_permit_root_login}
-
-	config{ "Port": value => $ssh_port }
-	config{ "PermitRootLogin": value => $ssh_permit_root_login}
-
-	nagios::service{ "ssh_port_${ssh_port}": check_command => "ssh_port!$ssh_port" }
-
+  
+  package {
+    "openssh-server":
+      ensure => installed,
+      require => File["/etc/ssh"],
+  }
+  
+  service {
+    ssh:
+      ensure => running,
+      pattern => "sshd",
+      require => Package["openssh-server"],
+  }
+  
+  # Now add the key, if we've got one
+  case $sshrsakey {
+    "": { 
+      notice("no sshrsakey on $fqdn")
+    }
+    default: {
+      @@sshkey { "$fqdn":
+	type => ssh-rsa,
+	key => $sshrsakey,
+	ensure => present,
+	require => Package["openssh-client"],
+      }
+    }
+  }
+  
+  $ssh_port = $ssh_port ? { '' => 22, default => $ssh_port }
+  $ssh_permit_root_login = $ssh_permit_root_login ? {'' => no, default => $ssh_permit_root_login}
+  
+  config{ "Port": value => $ssh_port }
+  config{ "PermitRootLogin": value => $ssh_permit_root_login}
+  
+  nagios::service{ "ssh_port_${ssh_port}": check_command => "ssh_port!$ssh_port" }
+  
 }
 
 define ssh::server::config($value) {
-	augeas {
-		"sshd_config_$name":
-		context =>  "/files/etc/ssh/sshd_config",
-		changes =>  "set $name $value",
-		onlyif  =>  "get $name != $value",
-		# onlyif  =>  "match $name/*[.='$value'] size == 0",
-	}
+  augeas {
+    "sshd_config_$name":
+      context =>  "/files/etc/ssh/sshd_config",
+      changes =>  "set $name $value",
+      onlyif  =>  "get $name != $value",
+      # onlyif  =>  "match $name/*[.='$value'] size == 0",
+  }
 }
