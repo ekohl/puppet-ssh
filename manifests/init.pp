e789499e71608b4a07c15069624d75e3aef91dab
Ewoud Kohl van Wijngaarden <e.kohlvanwijngaarden@oxilion.nl>
2011-07-27 17:14:36 +0200

    Fix whitespace

--- manifests/init.pp
+++ manifests/init.pp
@@ -17,55 +17,54 @@ class ssh::client inherits ssh::common {
       ensure => installed,
       require => File["/etc/ssh"],
   }
-  
 
   file {
     "/etc/ssh/ssh_known_hosts":
       mode => 0644, owner => root, group => 0;
   }
-  
+
   # Now collect all server keys
   Sshkey <<||>>
 }
 
 class ssh::server inherits ssh::client {
-  
+
   package {
     "openssh-server":
       ensure => installed,
       require => File["/etc/ssh"],
   }
-  
+
   service {
     ssh:
       ensure => running,
       pattern => "sshd",
       require => Package["openssh-server"],
   }
-  
+
   # Now add the key, if we've got one
   case $sshrsakey {
-    "": { 
+    "": {
       notice("no sshrsakey on $fqdn")
     }
     default: {
       @@sshkey { "$fqdn":
-	type => ssh-rsa,
-	key => $sshrsakey,
-	ensure => present,
-	require => Package["openssh-client"],
+        type => ssh-rsa,
+        key => $sshrsakey,
+        ensure => present,
+        require => Package["openssh-client"],
       }
     }
   }
-  
+
   $ssh_port = $ssh_port ? { '' => 22, default => $ssh_port }
   $ssh_permit_root_login = $ssh_permit_root_login ? {'' => no, default => $ssh_permit_root_login}
-  
+
   config{ "Port": value => $ssh_port }
   config{ "PermitRootLogin": value => $ssh_permit_root_login}
-  
+
   nagios::service{ "ssh_port_${ssh_port}": check_command => "ssh_port!$ssh_port" }
-  
+
 }
 
 define ssh::server::config($value) {
