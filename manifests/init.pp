f7096c33a82e72904c1b219eb3c436de273dadac
Alexander Werner <alex@documentfoundation.org>
2011-10-15 11:39:50 +0200

    Fixed operatingsystem name under Ubuntu/Debian

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,7 +5,7 @@
 
 class ssh::client::params {
   $packagename = $operatingsystem ? {
-    /debian|ubuntu/ => "openssh-client",
+    /Debian|Ubuntu/ => "openssh-client",
     /CentOS|rhel/   => "openssh-clients",
     default         => "openssh",
   }
